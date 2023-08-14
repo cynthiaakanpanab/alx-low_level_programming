b0fa686c99b35b1e59dd4fab9fb5e2e97457b073
cynthiaakanpanab <akanpanabcynthia@gmail.com>
2023-08-14 10:49:51 -0700

    removed 4-putd.v

--- 0x00-hello_world/4-putd.v
+++
@@ -1,8 +0,0 @@
-#include <stdio h>
-	** main - A program that prints a line with puts function
-	* Return: 0 (Success)
-	*
-	int main (void)
-	puts ("|"Programming is like building a multilingual puzz"):
-	Return (0);
-
