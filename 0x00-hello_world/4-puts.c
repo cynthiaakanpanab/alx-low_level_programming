0d81c6768bc4c1da400bd61d53f2b0268745a1e2
cynthiaakanpanab <akanpanabcynthia@gmail.com>
2023-08-14 12:25:56 +0000

    removed 6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include<stdio.h>
 /**
 *main - Entry point
@@ -7,5 +8,16 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
+=======
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("\'Programming is like building a multilingual puzzle");
+>>>>>>> 9366ec6 (4-puts.c)
 return (0);
 }
