967108a94cf877e57302412869314734dcc1d019
cynthiaakanpanab <akanpanabcynthia@gmail.com>
2023-08-23 06:13:49 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+
+/**
+* _strlen - Calculates the length of a string.
+* @s: Pointer to the string.
+*
+* Return: Length of the string.
+*/
+int _strlen(char *s)
+{
+int length = 0;
+
+while (*s != '\0')
+{
+length++;
+s++;
+}
+
+return (length);
+}
+
+/**
+* puts_half - Prints the second half of a string followed by a new line.
+* @str: Pointer to the string.
+*/
+void puts_half(char *str)
+{
+int length = _strlen(str);
+int start;
+
+if (length % 2 == 0)
+{
+start = length / 2;
+}
+else
+{
+start = (length - 1) / 2;
+}
+
+while (str[start] != '\0')
+{
+write(1, &str[start], 1);
+start++;
+}
+
+write(1, "\n", 1);
+}
