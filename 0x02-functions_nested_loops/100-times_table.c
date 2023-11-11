42bc9bd34f3ca09e85e867ff0197e7c12d502316
cynthiaakanpanab <akanpanabcynthia@gmail.com>
2023-11-11 10:47:11 +0000

    11-print_to_98.c updatd

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,48 +2,35 @@
 
 /**
 * print_times_table - prints the n times table, starting with 0
-* @n: number of the times table
+* @n: the value to generate the times table up to
 */
 void print_times_table(int n)
 {
-int i, j, k;
-
-if (n >= 0 && n <= 15)
-{
+int i, j, result;
+if (n < 0 || n > 15)
+return;
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-k = j * i;
+result = i * j;
 if (j == 0)
-{
-_putchar(k + '0');
-}
-else if (k < 10 && j != 0)
+_putchar(result + '0');
+else
 {
 _putchar(',');
 _putchar(' ');
+if (result < 10)
 _putchar(' ');
+if (result < 100)
 _putchar(' ');
-_putchar(k + '0');
+if (result >= 100)
+_putchar(result / 100 + '0');
+if (result >= 10)
+_putchar(result / 10 % 10 + '0');
+_putchar(result % 10 + '0');
 }
-else if (k >= 10 && k < 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar((k / 10) + '0');
-_putchar((k % 10) + '0');
-}
-else if (k >= 100)
-{
-_putchar(',');
-_putchar(' ');
-_putchar((k / 100) + '0');
-_putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');
 }
 _putchar('\n');
-}
 }
 }
